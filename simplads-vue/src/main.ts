b23ad5235e09f2d5233528c0623289715fad0c6d
Phi Huynh <trumhemcut@hotmail.com>
2020-01-03 22:46:49 +0700

    #36 Fix return uri

--- simplads-vue/src/main.ts
+++ simplads-vue/src/main.ts
@@ -1,28 +1,28 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import Adal from 'vue-adal'
-import Vuesax from 'vuesax'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import Adal from "vue-adal";
+import Vuesax from "vuesax";
 
-import 'vuesax/dist/vuesax.css' //Vuesax styles
+import "vuesax/dist/vuesax.css"; //Vuesax styles
 
-Vue.use(Vuesax, {})
+Vue.use(Vuesax, {});
 Vue.use(Adal, {
-    config: {
-      tenant: 'd8ac9062-da58-40c7-87f3-2ddea9fa470f',
-      clientId: 'a6e7c431-4570-4135-a3ba-bbf92b146833',
-      redirectUri: 'http://localhost:8080/',
-      cacheLocation: 'localStorage'
-    },
-    requireAuthOnInitialize: false,
-    router: router
-  });
+  config: {
+    tenant: process.env.VUE_APP_TENANT_ID,
+    clientId: process.env.VUE_APP_CLIENT_ID,
+    redirectUri: process.env.VUE_APP_REDIRECT_URI,
+    cacheLocation: "localStorage"
+  },
+  requireAuthOnInitialize: false,
+  router: router
+});
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
